16eb3491cb75e9eed340e8948f921033db6e1806
Eugene Yum <eugene.yum@leidos.com>
2021-06-29 22:24:18 -0400

    updates

--- src/typedefs.js
+++ src/typedefs.js
@@ -1,16 +1,20 @@
 module.exports = [`
     type Query {
+        # A query to get a specific user
         user(id: ID!): User
+        # A query to get a list of all users
         users: [User]
     }
     type Mutation {
+        # A mutation to update a user's information
         updateUserInfo(
-            id: ID!,
-            name: String, 
-            city: String, 
+            id: ID!
+            name: String
+            city: String
             state: String
         ): User
     }
+    # A data model representing a user
     type User {
         id: ID
         name: String
@@ -19,6 +23,7 @@ module.exports = [`
         followers: [User]
         posts: [Post]
     }
+    # A data model representing a post
     type Post {
         id: ID
         title: String
